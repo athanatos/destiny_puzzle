2de140dbe351bbd8116b61fff92fd1316db1ead3
Samuel Just <sjust@redhat.com>
2020-01-15 21:21:00 -0800

    first script

--- run.py
+++ run.py
@@ -26,6 +26,9 @@ def valid_name(x): return x in NAMES
 def get_open_edges(x):
     return [int(i) for i in x.strip().split(',')]
 
+EMPTY = 'bbbbbbb'
+def null_edge(x): return x == EMPTY
+
 def read_file(filename):
     ret = []
     with open(filename) as tsvfile:
@@ -40,21 +43,97 @@ def read_file(filename):
             edges = line[2:]
 
             if not valid_name(center):
-                print("{} invalid", center)
                 continue
 
             open_sides_decoded = None
             try:
                 open_sides_decoded = get_open_edges(open_sides)
             except:
-                print("{} invalid", open_sides)
                 continue
 
-            if not all((all(map(valid_symbol, x)) for x in edges)):
-                print("{} invalid", edges)
+            if not all((len(x) == 7 and all(map(valid_symbol, x)) for x in edges)):
                 continue
             
             ret.append((center, open_sides_decoded, edges))
     return ret
 
-[print(i) for i in read_file('initial_sheet.tsv')]
+data = read_file('initial_sheet.tsv')
+
+def count_edges(x):
+    edge_counts = {}
+    for _, _, edges in x:
+        for edge in edges:
+            if null_edge(edge): continue
+            if edge not in edge_counts:
+                edge_counts[edge] = 0
+            edge_counts[edge] += 1
+    return list(sorted((count, edge) for edge, count in edge_counts.items()))
+
+class Node(object):
+    def __init__(self, symbol, open_edges, edges):
+        self.__symbol = symbol
+        self.__open_edges = open_edges
+        self.__edges = edges
+        self.__neighbors = [None for x in self.__edges]
+        self.__tag = None
+
+    def __repr__(self):
+        return "Node(symbol={}, open_edges={}, edges={}".format(
+            self.__symbol, self.__open_edges, self.__edges)
+
+    def symbol(self):
+        return self.__symbol
+
+    def get_edges(self):
+        return self.__edges
+
+    def set_neighbors(self, edge_to_nodes):
+        for idx, edge in zip(range(len(self.__edges)), self.__edges):
+            if null_edge(edge):
+                continue
+            nodes = edge_to_nodes[edge]
+            if len(nodes) > 2:
+                print("{} has 3 nodes: {}", edge, nodes)
+            for node in nodes:
+                if node is not self:
+                    self.__neighbors[idx] = node
+
+    def get_tag(self):
+        return self.__tag
+
+    def tagged(self):
+        return self.__tag is not None
+
+    def visit(self, tag):
+        self.__tag = tag
+        nodes = [self]
+        for neighbor in self.__neighbors:
+            if neighbor is not None and not neighbor.tagged():
+                nodes += neighbor.visit(tag)
+        return nodes
+
+
+nodes = [Node(*x) for x in data]
+
+edge_to_nodes = {}
+for node in nodes:
+    for edge in node.get_edges():
+        if null_edge(edge):
+            continue
+        if edge not in edge_to_nodes:
+            edge_to_nodes[edge] = []
+        edge_to_nodes[edge].append(node)
+
+[x.set_neighbors(edge_to_nodes) for x in nodes]
+
+tag = 0
+tags = {}
+for node in nodes:
+    if node.tagged():
+        continue
+    nodes = node.visit(tag)
+    tags[tag] = (len(nodes), nodes)
+    tag += 1
+
+[print(x) for x in sorted(((num, tag, [x.symbol() for x in nodes]) for tag, (num, nodes) in tags.items()))]
+
